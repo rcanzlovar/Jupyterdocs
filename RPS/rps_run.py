e94ec21a4d0098054fa365eb8ca47cd0cc2ee9c3
Bob Anzlovar <rcanzlovar@gmail.com>
2020-06-15 15:52:02 -0600

    addd the rock paper scissor

---
+++ RPS/rps_run.py
@@ -0,0 +1,21 @@
+%matplotlib inline
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+pic_index = 2
+
+next_rock = [os.path.join(rock_dir, fname) 
+                for fname in rock_files[pic_index-2:pic_index]]
+next_paper = [os.path.join(paper_dir, fname) 
+                for fname in paper_files[pic_index-2:pic_index]]
+next_scissors = [os.path.join(scissors_dir, fname) 
+                for fname in scissors_files[pic_index-2:pic_index]]
+
+for i, img_path in enumerate(next_rock+next_paper+next_scissors):
+  #print(img_path)
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+  plt.axis('Off')
+  plt.show()
+
